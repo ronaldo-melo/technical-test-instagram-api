a5e72e7bd401ed54fed87e0e479aa3216cfe044b
Ronaldo Melo <rpm.melkor@gmail.com>
2020-08-29 10:10:22 -0300

    FollowedUser entity

---
+++ src/main/java/com/br/instagram/model/FollowedUser.java
@@ -0,0 +1,35 @@
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class FollowedUser { // USUÁRIO SEGUIDO
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column
+	private Long userWhoWillBeAFollowerId; // ID DO USUÁRIO SERÁ SEU SEGUIDOR
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getUserWhoWillBeAFollowerId() {
+		return userWhoWillBeAFollowerId;
+	}
+
+	public void setUserWhoWillBeAFollowerID(Long userWhoWillBeAFollowerID) {
+		this.userWhoWillBeAFollowerId = userWhoWillBeAFollowerID;
+	}
+
+}
