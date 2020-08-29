208d1e7a69517a693df9c8b4edd4e3c955cf8c0e
Ronaldo Melo <rpm.melkor@gmail.com>
2020-08-29 15:00:46 -0300

    Endpoint - save user

--- src/main/java/com/br/instagram/controller/UserController.java
+++ src/main/java/com/br/instagram/controller/UserController.java
@@ -4,9 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,5 +36,12 @@ public class UserController {
 		return ResponseEntity.of(userOptinal);
 
 	}
+	
+	@PostMapping
+	public ResponseEntity<User> save(@RequestBody User user) {
+		user = userRepository.save(user);
+		return ResponseEntity.status(HttpStatus.CREATED).body(user);
+	}
+
 
 }
