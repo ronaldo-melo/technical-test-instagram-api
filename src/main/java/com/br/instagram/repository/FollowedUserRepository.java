1a0740a87a773b36021c0640fa08bff75999cb87
Ronaldo Melo <rpm.melkor@gmail.com>
2020-08-29 12:20:59 -0300

    FollowedUserRepository

---
+++ src/main/java/com/br/instagram/repository/FollowedUserRepository.java
@@ -0,0 +1,9 @@
+package com.br.instagram.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.br.instagram.model.FollowedUser;
+
+public interface FollowedUserRepository extends JpaRepository<FollowedUser, Long> {
+
+}
