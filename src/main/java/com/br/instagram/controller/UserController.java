e6af6a3fe9ceba555941aef4416b2943249a0f5b
Ronaldo Melo <rpm.melkor@gmail.com>
2020-08-29 13:28:35 -0300

    Endpoint - find user by id

--- src/main/java/com/br/instagram/controller/UserController.java
+++ src/main/java/com/br/instagram/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.br.instagram.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,13 +16,22 @@ import com.br.instagram.repository.UserRepositoty;
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
-	
+
 	@Autowired
 	private UserRepositoty userRepository;
 
 	@GetMapping
-	public List<User> findAll(){
+	public List<User> findAll() {
 		return userRepository.findAll();
 	}
-	
+
+	@GetMapping(value = "/{userId}")
+	public ResponseEntity<User> findAll(@PathVariable Long userId) {
+
+		Optional<User> userOptinal = userRepository.findById(userId);
+
+		return ResponseEntity.of(userOptinal);
+
+	}
+
 }
