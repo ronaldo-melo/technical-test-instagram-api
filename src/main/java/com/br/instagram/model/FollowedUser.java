db7cd2365fd12f302c7afbd05e766ca1ba3fec37
Ronaldo Melo <rpm.melkor@gmail.com>
2020-08-29 10:59:40 -0300

    correção no nome do pacote

--- src/main/java/com/br/instagram/model/FollowedUser.java
+++ src/main/java/com/br/instagram/model/FollowedUser.java
@@ -1,10 +1,9 @@
+package com.br.instagram.model;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 
 @Entity
 public class FollowedUser { // USUÁRIO SEGUIDO
