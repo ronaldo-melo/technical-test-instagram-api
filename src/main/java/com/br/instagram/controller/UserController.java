ee1555ad0857a67e484632fef72388a15614e517
Ronaldo Melo <rpm.melkor@gmail.com>
2020-08-29 13:18:22 -0300

    Endpoint - findAll users

--- src/main/java/com/br/instagram/controller/UserController.java
+++ src/main/java/com/br/instagram/controller/UserController.java
@@ -1,12 +1,25 @@
 package com.br.instagram.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.br.instagram.model.User;
+import com.br.instagram.repository.UserRepositoty;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
 	
-	
+	@Autowired
+	private UserRepositoty userRepository;
 
+	@GetMapping
+	public List<User> findAll(){
+		return userRepository.findAll();
+	}
+	
 }
