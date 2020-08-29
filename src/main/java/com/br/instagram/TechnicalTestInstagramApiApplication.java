bf22284cfef59d24cab3967e8de7fcf1aa8a0b4a
Ronaldo Melo <rpm.melkor@gmail.com>
2020-08-29 09:54:15 -0300

    Project

---
+++ src/main/java/com/br/instagram/TechnicalTestInstagramApiApplication.java
@@ -0,0 +1,13 @@
+package com.br.instagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalTestInstagramApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnicalTestInstagramApiApplication.class, args);
+	}
+
+}
