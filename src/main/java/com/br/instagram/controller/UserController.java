a229053523cc2ed0e0208893ed4de1e0866283ff
Ronaldo Melo <rpm.melkor@gmail.com>
2020-08-30 11:44:08 -0300

    Endpoint user - unfollow user

--- src/main/java/com/br/instagram/controller/UserController.java
+++ src/main/java/com/br/instagram/controller/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -32,13 +33,13 @@ public class UserController {
 
 	@Autowired
 	private UserRepositoty userRepository;
-	
+
 	@Autowired
 	private FollowedUserRepository followedUserRepository;
-	
+
 	@Autowired
 	private FollowerUserRepository followerUserRepository;
-	
+
 	@GetMapping
 	public List<User> findAll() {
 		return userRepository.findAll();
@@ -52,7 +53,7 @@ public class UserController {
 		return ResponseEntity.of(userOptinal);
 
 	}
-	
+
 	@PostMapping
 	public ResponseEntity<User> save(@RequestBody User user) {
 		user = userRepository.save(user);
@@ -63,155 +64,161 @@ public class UserController {
 	public ResponseEntity<?> deleteById(@PathVariable Long userId) {
 
 		try {
-			
+
 			userRepository.deleteById(userId);
-			
+
 			return ResponseEntity.noContent().build();
 
 		} catch (EmptyResultDataAccessException e) {
-			
+
 			return ResponseEntity.notFound().build();
 
 		}
 
 	}
-	
+
 	@PutMapping(value = "/{followUserId}/follow-user")
 	public ResponseEntity<?> followUser(@PathVariable Long followUserId, @RequestBody User userWhoWillBeAFollower) {
-		
-		//verifica se o seguidor existe
-		Optional<User> followUserOptional = userRepository.findById(followUserId);				
-		if(followUserOptional.isEmpty())
+
+		// verifica se o seguidor existe
+		Optional<User> followUserOptional = userRepository.findById(followUserId);
+		if (followUserOptional.isEmpty())
 			return ResponseEntity.notFound().build();
-		
-		//verifica se o usuário a ser seguido existe
-		Optional<User> userToBeFollowedOptional = userRepository.findById(userWhoWillBeAFollower.getId());		
-		if(userToBeFollowedOptional.isEmpty())
+
+		// verifica se o usuário a ser seguido existe
+		Optional<User> userToBeFollowedOptional = userRepository.findById(userWhoWillBeAFollower.getId());
+		if (userToBeFollowedOptional.isEmpty())
 			return ResponseEntity.badRequest().build();
-		
-		/*	
-		    Impedir que um mesmo usuário seja seguido duas vezes pela mesma pessoa. 
-		 	No caso entre os registros da tabela de Followed Users se haver um registro 
-		 	com os valores userWhoWillBeAFollower(usuário que será seguidor) e
-		 	followUserId (usuário seguido) então haverá um conflito pois não 
-		 	faz sentido uma pessoa seguir outra mais de uma vez.	   
-		*/
-		
-		if (followedUserRepository.findAll()
-				.stream()
-					//verifica se um usuário que segue já existe ...
-					.anyMatch((u) -> u.getFollowedUser().getId() == followUserId
-					//com o mesmo id de usuário a ser seguido
-					&& userWhoWillBeAFollower.getId() == u.getUserWhoWillBeAFollowerId())
-				) {
-			
+
+		/*
+		 * Impedir que um mesmo usuário seja seguido duas vezes pela mesma pessoa. No
+		 * caso entre os registros da tabela de Followed Users se haver um registro com
+		 * os valores userWhoWillBeAFollower(usuário que será seguidor) e followUserId
+		 * (usuário seguido) então haverá um conflito pois não faz sentido uma pessoa
+		 * seguir outra mais de uma vez.
+		 */
+
+		if (followedUserRepository.findAll().stream()
+				// verifica se um usuário que segue já existe ...
+				.anyMatch((u) -> u.getFollowedUser().getId() == followUserId
+						// com o mesmo id de usuário a ser seguido
+						&& userWhoWillBeAFollower.getId() == u.getUserWhoWillBeAFollowerId())) {
+
 			return ResponseEntity.status(HttpStatus.CONFLICT).body("Não pode seguir um usuário que já é seguido!");
-			
+
 		} else {
-			
-			//Usuário seguido
+
+			// Usuário seguido
 			FollowedUser followedUser = new FollowedUser();
-			
+
 			followedUser.setFollowedUser(followUserOptional.get());
-			
+
 			followedUser.setUserWhoWillBeAFollowerID(userToBeFollowedOptional.get().getId());
-			
+
 			followedUserRepository.save(followedUser);
-			
+
 			/*
-			
-				Quando um usuário é seguido ele ganha (obviamente ele ganha um seguidor (FollowerUser))
-				por isso followUserOptional.get() será o usuário que seguidor e 
-				userToBeFollowedOptional.get().getId() é id do usuário a ser seguido ou que foi seguido
-				AO AVALIADOR QUE ESTIVER LENDO ESSE CÓDIGO SAIBA ESSA
-				RELAÇÃO UM PRA MUITOS ENTRE User e FollowerUser/FollowedUser FOI O QUE
-				ME DEU MAIS TRABALHO.
-			  
-			*/
-			
+			 * 
+			 * Quando um usuário é seguido ele ganha (obviamente ele ganha um seguidor
+			 * (FollowerUser)) por isso followUserOptional.get() será o usuário que seguidor
+			 * e userToBeFollowedOptional.get().getId() é id do usuário a ser seguido ou que
+			 * foi seguido AO AVALIADOR QUE ESTIVER LENDO ESSE CÓDIGO SAIBA ESSA RELAÇÃO UM
+			 * PRA MUITOS ENTRE User e FollowerUser/FollowedUser FOI O QUE ME DEU MAIS
+			 * TRABALHO.
+			 * 
+			 */
+
 			addFollow(followUserOptional.get(), userToBeFollowedOptional.get().getId());
-			
+
 		}
-		
+
 		return ResponseEntity.noContent().build();
 	}
-	
+
 	private void addFollow(User followerUserId, Long userToBeFolloweId) {
-		
+
 		FollowerUser followerUser = new FollowerUser();
 		followerUser.setFollowerUser(followerUserId);
 		followerUser.setFollowerUserId(userToBeFolloweId);
-		
+
 		followerUserRepository.save(followerUser);
 	}
-	
-	//Ver os seguidores de um usuário a partir do Id
+
+	// Ver os seguidores de um usuário a partir do Id
 	@GetMapping("/following/{followerUserId}")
-	public List<User> following(@PathVariable Long followerUserId){
-		
-		//Retorna a lista de seguidos do usuário de id followerUserId(seguidor). As PoessiaS que ele segue
-		List<FollowedUser> followedUsers = followedUserRepository.findAll()
-				.stream()
-					.filter(fu -> fu.getFollowedUser().getId() == followerUserId)
-				.collect(Collectors.toList());
-		
-		//extrair oS ids das pessoas seguidas 
-		Set<Long> followingIDs = followedUsers
-				.stream()
-					.map(fu -> fu.getUserWhoWillBeAFollowerId())
+	public List<User> following(@PathVariable Long followerUserId) {
+
+		// Retorna a lista de seguidos do usuário de id followerUserId(seguidor). As
+		// PoessiaS que ele segue
+		List<FollowedUser> followedUsers = followedUserRepository.findAll().stream()
+				.filter(fu -> fu.getFollowedUser().getId() == followerUserId).collect(Collectors.toList());
+
+		// extrair oS ids das pessoas seguidas
+		Set<Long> followingIDs = followedUsers.stream().map(fu -> fu.getUserWhoWillBeAFollowerId())
 				.collect(Collectors.toSet());
-		
-		//comparar os ids com os de todos os usuário PARA retornar apenas os usuários que tenham id igual ao presente no Set followingIDs
-		//outra solução usar a operação contains de followingIDs para user de allUsers em um predicade
-		List<User> following = userRepository.findAll()
-				.stream()
-					.filter((u) -> followingIDs.contains(u.getId()))
+
+		// comparar os ids com os de todos os usuário PARA retornar apenas os usuários
+		// que tenham id igual ao presente no Set followingIDs
+		// outra solução usar a operação contains de followingIDs para user de allUsers
+		// em um predicade
+		List<User> following = userRepository.findAll().stream().filter((u) -> followingIDs.contains(u.getId()))
 				.collect(Collectors.toList());
-		
+
 		return following;
-		
+
 	}
 
-	//ver os seguidores de um usuário
+	// ver os seguidores de um usuário
 	@GetMapping("/followers/{followingUserId}")
-	public List<User> followers(@PathVariable Long followingUserId){
-		
-		//Retorna a lista de seguidos do usuário de id followerUserId(seguidor). As pessoas que ele segue
-		List<FollowerUser> followersUsers = followerUserRepository.findAll()
-				.stream()
-					.filter(fu -> fu.getFollowerUser().getId() == followingUserId)
+	public List<User> followers(@PathVariable Long followingUserId) {
+
+		// Retorna a lista de seguidos do usuário de id followerUserId(seguidor). As
+		// pessoas que ele segue
+		List<FollowerUser> followersUsers = followerUserRepository.findAll().stream()
+				.filter(fu -> fu.getFollowerUser().getId() == followingUserId).collect(Collectors.toList());
+
+		// extrair oS ids das pessoas seguidas
+		Set<Long> followingIDs = followersUsers.stream().map(fu -> fu.getFollowerUserId()).collect(Collectors.toSet());
+
+		// comparar os ids com os de todos os usuário PARA retornar apenas os usuários
+		// que tenham id igual
+		// ao presente no Set followingIDs
+		List<User> following = userRepository.findAll().stream().filter((u) -> followingIDs.contains(u.getId()))
 				.collect(Collectors.toList());
-		
-		//extrair oS ids das pessoas seguidas 
-		Set<Long> followingIDs = followersUsers
-				.stream()
-					.map(fu -> fu.getFollowerUserId())
-				.collect(Collectors.toSet());
-		
-		//comparar os ids com os de todos os usuário PARA retornar apenas os usuários que tenham id igual
-		//ao presente no Set followingIDs
-		List<User> following = userRepository.findAll()
-				.stream()
-					.filter((u) -> followingIDs.contains(u.getId()))
-				.collect(Collectors.toList());
-		
+
 		return following;
-		
+
 	}
 
-	//ver informações de perfil de um usuário (nome, nome de usuáio, total de seguidores, total de seguidos) 
+	// ver informações de perfil de um usuário (nome, nome de usuáio, total de
+	// seguidores, total de seguidos)
 	@GetMapping(value = "profile/{userId}")
-	public ResponseEntity<?> userProfile(@PathVariable Long userId){
-		
+	public ResponseEntity<?> userProfile(@PathVariable Long userId) {
+
 		Optional<User> userOptional = userRepository.findById(userId);
-		
-		if(userOptional.isEmpty())
+
+		if (userOptional.isEmpty())
 			return ResponseEntity.notFound().build();
-		
+
 		User user = userOptional.get();
-		
+
 		UserDTO userDTO = new UserDTO(user, following(user.getId()), followers(user.getId()));
-		
+
 		return ResponseEntity.ok(userDTO);
+	}
+
+	@PatchMapping(value = "/unfollow/{userId}/{unfollowUserId}")
+	public ResponseEntity<?> unfollowUser(@PathVariable Long userId, @PathVariable Long unfollowUserId) {
+
+		Optional <FollowedUser> unfollowID = followedUserRepository.findAll()
+				.stream()
+					.filter(x -> x.getUserWhoWillBeAFollowerId() == unfollowUserId)
+					.findAny();
+
+		Long id = unfollowID.get().getId();
+		
+		followedUserRepository.deleteById(id);
+		
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/br/instagram/repository/FollowedUserRepository.java
+++ src/main/java/com/br/instagram/repository/FollowedUserRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.br.instagram.model.FollowedUser;
 
 public interface FollowedUserRepository extends JpaRepository<FollowedUser, Long> {
-
+	
 }
